4425cf9c8b971755dc303b199fb8dadb6196dabb
Milan141113 <79976666+Milan141113@users.noreply.github.com>
2024-04-05 12:38:24 +0300

    база

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 import mongoose from 'mongoose';
-import { registerValidation } from './validations/auth';
+import { registerValidation } from './validations/auth.js';
 import { validationResult } from 'express-validator';
+import UserModel from './modules/User.js';
 
 mongoose
-  .connect(`mongodb+srv://milan:61-606mir@cluster0.w9rhs99.mongodb.net/`)
+  .connect(`mongodb+srv://milan:61-606mir@cluster0.w9rhs99.mongodb.net/blog`)
   .then(() => {
     console.log('Подключение к Mongo успешно');
   })
-  .catch((err) => {
-    console.log(err);
-  });
-
+  .catch((err) => console.log("Database can't connect!", err));
 const app = express();
 
 app.use(express.json());
 
-app.post('/auth/regiser', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
-  res.json({
-    succsess: true,
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
   });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(4444, (err) => {
